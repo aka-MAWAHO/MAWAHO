98dd5143a2439a606407f5610d6dead9a68291d0
waliid-sliim <walidovicc@gmail.com>
2022-12-22 16:41:49 +0100

    about us

--- aka/pages/aboutus.tsx
+++ aka/pages/aboutus.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function aboutus() {
   return (
     <div className="about">
-      <h6>
+      <h4>
         MAWAHO GROUP is one of the leading international fashion companies. It
         belongs to Inditex, one of the largest distribution groups in the world.
         <p>
@@ -12,7 +12,7 @@ function aboutus() {
           design, manufacturing, distribution and sales through an extensive
           network of branded stores.
         </p>{" "}
-      </h6>
+      </h4>
       <section className="contact" id="contact">
   <h1 className="heading">
     {" "}
@@ -61,7 +61,122 @@ function aboutus() {
 
   </div>
 </section>
-
+<section>
+  <div className="row d-flex justify-content-center">
+    <div className="col-md-10 col-xl-8 text-center">
+      <h3 className="mb-4">Testimonials</h3>
+      <p className="mb-4 pb-2 mb-md-5 pb-md-0">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error
+        amet numquam iure provident voluptate esse quasi, veritatis totam
+        voluptas nostrum quisquam eum porro a pariatur veniam.
+      </p>
+    </div>
+  </div>
+  <div className="row text-center">
+    <div className="col-md-4 mb-5 mb-md-0">
+      <div className="d-flex justify-content-center mb-4">
+        <img
+          src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).webp"
+          className="rounded-circle shadow-1-strong"
+          width={150}
+          height={150}
+        />
+      </div>
+      <h5 className="mb-3">Maria Smantha</h5>
+      <h6 className="text-primary mb-3">Web Developer</h6>
+      <p className="px-xl-3">
+        <i className="fas fa-quote-left pe-2" />
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos id
+        officiis hic tenetur quae quaerat ad velit ab hic tenetur.
+      </p>
+      <ul className="list-unstyled d-flex justify-content-center mb-0">
+        <li>
+          <i className="fas fa-star fa-sm text-warning" />
+        </li>
+        <li>
+          <i className="fas fa-star fa-sm text-warning" />
+        </li>
+        <li>
+          <i className="fas fa-star fa-sm text-warning" />
+        </li>
+        <li>
+          <i className="fas fa-star fa-sm text-warning" />
+        </li>
+        <li>
+          <i className="fas fa-star-half-alt fa-sm text-warning" />
+        </li>
+      </ul>
+    </div>
+    <div className="col-md-4 mb-5 mb-md-0">
+      <div className="d-flex justify-content-center mb-4">
+        <img
+          src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp"
+          className="rounded-circle shadow-1-strong"
+          width={150}
+          height={150}
+        />
+      </div>
+      <h5 className="mb-3">Lisa Cudrow</h5>
+      <h6 className="text-primary mb-3">Graphic Designer</h6>
+      <p className="px-xl-3">
+        <i className="fas fa-quote-left pe-2" />
+        Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
+        suscipit laboriosam, nisi ut aliquid commodi.
+      </p>
+      <ul className="list-unstyled d-flex justify-content-center mb-0">
+        <li>
+          <i className="fas fa-star fa-sm text-warning" />
+        </li>
+        <li>
+          <i className="fas fa-star fa-sm text-warning" />
+        </li>
+        <li>
+          <i className="fas fa-star fa-sm text-warning" />
+        </li>
+        <li>
+          <i className="fas fa-star fa-sm text-warning" />
+        </li>
+        <li>
+          <i className="fas fa-star fa-sm text-warning" />
+        </li>
+      </ul>
+    </div>
+    <div className="col-md-4 mb-0">
+      <div className="d-flex justify-content-center mb-4">
+        <img
+          src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(9).webp"
+          className="rounded-circle shadow-1-strong"
+          width={150}
+          height={150}
+        />
+      </div>
+      <h5 className="mb-3">John Smith</h5>
+      <h6 className="text-primary mb-3">Marketing Specialist</h6>
+      <p className="px-xl-3">
+        <i className="fas fa-quote-left pe-2" />
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui
+        blanditiis praesentium voluptatum deleniti atque corrupti.
+      </p>
+      <ul className="list-unstyled d-flex justify-content-center mb-0">
+        <li>
+          <i className="fas fa-star fa-sm text-warning" />
+        </li>
+        <li>
+          <i className="fas fa-star fa-sm text-warning" />
+        </li>
+        <li>
+          <i className="fas fa-star fa-sm text-warning" />
+        </li>
+        <li>
+          <i className="fas fa-star fa-sm text-warning" />
+        </li>
+        <li>
+          <i className="far fa-star fa-sm text-warning" />
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
     </div>
     
   );
